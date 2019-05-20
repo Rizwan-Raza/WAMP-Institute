354f84230ac4a52d0f3a42066528f227d612f38c
Rizwan Raza <rizwan.raza987@gmail.com>
2019-05-20 17:19:57 +0530

    Addons Added

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -1,6 +1,78 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118454694-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-118454694-2');
+</script>
+<!-- - - -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<!-- pinterset -->
+<meta name="p:domain_verify" content="8fec2fac0163d466d3e4da14a3bda207" />
+<!-- google webmaster verification code -->
+<meta name="google-site-verification" content="1RKsZD-IpVcI-JiXnqcFdfToV-geikyzdPgUGn0V_Ho" />
+
+<meta name="google-site-verification" content="1RKsZD-IpVcI-JiXnqcFdfToV-geikyzdPgUGn0V_Ho" />
+
+
+<!-- Facebook Pixel Code -->
+<script>
+    if (typeof fbq === 'undefined') {
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f.fbq) f.fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '674076782768921');
+        fbq('track', 'PageView');
+    }
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=674076782768921&ev=PageView&noscript=1" /></noscript>
+<!-- End Facebook Pixel Code -->
+
+<!-- Bing Tag Manager -->
+<meta name="msvalidate.01" content="83ED7B6BD3E43D1BBA0211D134362961" />
+
+<!-- Google Tag Manager -->
+<script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-5NVML8W');
+</script>
+<!-- End Google Tag Manager -->
+
 
 <!-- favicons -->
 <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
@@ -21,14 +93,14 @@
 <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
-<meta name="thumbnail" content="https://www.wampinfotech.com/images/wamp-logo.png" />
+<meta name="thumbnail" content="https://www.wampinstitute.in/images/wamp-logo.png" />
 
 
-<meta itemprop="thumbnailUrl" content="https://www.wampinfotech.com/images/wamp-logo.png" />
+<meta itemprop="thumbnailUrl" content="https://www.wampinstitute.in/images/wamp-logo.png" />
 <meta property="og:site_name" content="WAMP Infotech" />
 
-<meta property="og:image" content="https://www.wampinfotech.com/images/wamp-logo.png" />
-<meta property="og:image:secure_url" content="https://www.wampinfotech.com/images/wamp-logo.png" />
+<meta property="og:image" content="https://www.wampinstitute.in/images/wamp-logo.png" />
+<meta property="og:image:secure_url" content="https://www.wampinstitute.in/images/wamp-logo.png" />
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:width" content="512" />
 <meta property="og:image:height" content="512" />
@@ -39,11 +111,11 @@
 <meta property="og:video:width" content="480" />
 <meta property="og:video:height" content="320" />
 
-<meta name="twitter:image" content="https://www.wampinfotech.com/images/wamp-logo.png">
+<meta name="twitter:image" content="https://www.wampinstitute.in/images/wamp-logo.png">
 
-<meta name="image" content="https://www.wampinfotech.com/images/wamp-logo.png">
+<meta name="image" content="https://www.wampinstitute.in/images/wamp-logo.png">
 
-<link rel="image_src" href="https://www.wampinfotech.com/images/wamp-logo.png" />
+<link rel="image_src" href="https://www.wampinstitute.in/images/wamp-logo.png" />
 
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -51,4 +123,5 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <!-- <link rel="stylesheet" href="css/helper-classes.css" /> -->
 <link rel="stylesheet" href="css/common.css" />
-<?php // require_once "includes/common.main.css.php"; ?>
+<?php
+?>
